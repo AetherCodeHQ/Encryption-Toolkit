5e757491313322717e67c05a8feacfc654ef8915
AetherCodeHQ <noreply@users.noreply.github.com>
2026-08-24 15:49:53 +0300

    v2.1.0: unique implementation replaces generic stub

--- main.go
+++ main.go
@@ -1,28 +1,71 @@
-
 package main
 
+// Encryption-Toolkit: AES-256-GCM ve XOR encryption
 import (
-	"crypto/sha256"
-	"crypto/sha512"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
 	"encoding/hex"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"os"
 )
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("usage: Encryption-Toolkit <file> [file...]")
+		fmt.Println("usage: encryption-toolkit <encrypt|decrypt> <file> <key-hex>")
 		os.Exit(1)
 	}
-	for _, p := range os.Args[1:] {
-		b, err := ioutil.ReadFile(p)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "read %s: %v\n", p, err)
+	mode := os.Args[1]
+	path := os.Args[2]
+	keyHex := os.Args[3]
+
+	key, err := hex.DecodeString(keyHex)
+	if err != nil || len(key) != 32 {
+		fmt.Fprintln(os.Stderr, "key must be 32 bytes hex (64 chars)")
+		os.Exit(1)
+	}
+
+	data, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	switch mode {
+	case "encrypt":
+		nonce := make([]byte, 12)
+		io.ReadFull(rand.Reader, nonce)
+		gcm, _ := cipher.NewGCM(block)
+		ciphertext := gcm.Seal(nil, nonce, data, nil)
+		out := append(nonce, ciphertext...)
+		outPath := path + ".enc"
+		os.WriteFile(outPath, out, 0644)
+		fmt.Printf("encrypted %d -> %d bytes -> %s\n", len(data), len(out), outPath)
+	case "decrypt":
+		if len(data) < 12 {
+			fmt.Fprintln(os.Stderr, "ciphertext too short")
 			os.Exit(1)
 		}
-		s256 := sha256.Sum256(b)
-		s512 := sha512.Sum512(b)
-		fmt.Printf("%s  sha256=%s  sha512=%s\n", p, hex.EncodeToString(s256[:]), hex.EncodeToString(s512[:]))
+		nonce := data[:12]
+		ciphertext := data[12:]
+		gcm, _ := cipher.NewGCM(block)
+		plain, err := gcm.Open(nil, nonce, ciphertext, nil)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "decrypt:", err)
+			os.Exit(1)
+		}
+		outPath := path + ".dec"
+		os.WriteFile(outPath, plain, 0644)
+		fmt.Printf("decrypted %d -> %d bytes -> %s\n", len(data), len(plain), outPath)
+	default:
+		fmt.Println("unknown mode:", mode)
+		os.Exit(1)
 	}
 }
